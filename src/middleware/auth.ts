77de12b72906aecc9078d781f1bb85681bd4a090
Victor Torres Moreno <victor.torres_taking@totalexpress.com.br>
2021-12-28 19:42:24 -0300

    added auth middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import { verify, decode } from "jsonwebtoken";
+
+export async function authentication(
+    request: Request,
+    response: Response,
+    next: NextFunction
+) {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+        return response.status(401).end();
+    }
+
+    const [, token] = authHeader.split(" ");
+
+    try {
+        verify(token, process.env.JWT_SECRET);
+
+        const { sub: userId } = decode(token);
+
+        request.userId = String(userId);
+
+        return next();
+    } catch (err) {
+        console.log(err)
+        return response.status(401).end();
+    }
+}
